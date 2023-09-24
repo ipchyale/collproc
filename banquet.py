87277dd944836afbe7af1b3874a5bef6209fbd17
damoncrockett <damoncrockett@hotmail.com>
2023-09-24 12:37:17 -0400

    broadcasting and set optimizations

--- banquet.py
+++ banquet.py
@@ -24,43 +24,16 @@ We choose the DBSCAN clustering with the minimum value of epsilon that satisfies
 constraint. Other DBSCAN parameters are chosen by the user, and we usually set min_samples=2.
 """
 
-def get_distance(collection_frame,collection_index,reference_frame,reference_index,feature_columns):
+def get_guest_lists(collection_frame, reference_frame, feature_columns, d):
     """
-    Returns Euclidean distance between reference item and collection item, 
-    in feature space with axes 'feature_columns'.
+    Vectorized computation of guest lists using broadcasting
     """
-    
-    collection_array = np.array(collection_frame[feature_columns].loc[collection_index])
-    reference_array = np.array(reference_frame[feature_columns].loc[reference_index])
-
-    return np.linalg.norm(collection_array-reference_array)
-
-def get_guest_list(collection_frame,reference_frame,collection_index,feature_columns,d):
-    """
-    Runs get_distance() on every reference item for a single collection item. Returns a 
-    list of indices from reference_frame of reference items that are "guests" of the 
-    collection item, where a "guest" is a reference item that is closer to the collection 
-    item than distance d.
-    """
-
-    guest_list = []
-    for reference_index in reference_frame.index:
-        if get_distance(collection_frame,collection_index,reference_frame,reference_index,feature_columns) < d:
-            guest_list.append(reference_index)
-    
-    return guest_list
-    
-def get_guest_lists(collection_frame,reference_frame,feature_columns,d):
-    """
-    Runs get_guest_list() on every collection item. Returns a pandas Series with the same
-    indices as collection_frame, where each element is a list of indices from reference_frame
-    """
-
-    guest_lists = []
-    for collection_index in collection_frame.index:
-        guest_lists.append(get_guest_list(collection_frame,reference_frame,collection_index,feature_columns,d))
-    
-    return pd.Series(guest_lists,index=collection_frame.index)
+    collection_array = collection_frame[feature_columns].values
+    reference_array = reference_frame[feature_columns].values
+    distances = np.linalg.norm(collection_array[:, np.newaxis] - reference_array, axis=2)
+    reference_indices = reference_frame.index.values
+    guest_lists = [list(reference_indices[row < d]) for row in distances]
+    return pd.Series(guest_lists, index=collection_frame.index)
 
 def get_allowable_tablemates_lists(guest_lists):
     """
@@ -69,72 +42,63 @@ def get_allowable_tablemates_lists(guest_lists):
     allowable tablemates if A and B have at least one guest in common. Thus, A and B are allowable
     tablemates if the intersection of their guest lists is nonempty.
 
-    We do not include a collection item in its own list of allowable tablemates.
+    The allowable tablemates list for a collection item does not include the collection item itself;
+    if we leave them in, "outsiders" are impossible.
     """
-    
-    allowable_tablemates_lists = []
-    for collection_index in guest_lists.index:
-        allowable_tablemates = []
-        for other_collection_index in guest_lists.index:
-            if collection_index != other_collection_index:
-                if len(set(guest_lists[collection_index]).intersection(set(guest_lists[other_collection_index]))) > 0:
-                    allowable_tablemates.append(other_collection_index)
-        allowable_tablemates_lists.append(allowable_tablemates)
 
-    return pd.Series(allowable_tablemates_lists,index=guest_lists.index)
+    guest_sets = guest_lists.apply(set)
+    indices = guest_lists.index
+    allowable_tablemates_lists = [
+        [idx for idx in indices if idx != collection_idx and guest_sets[collection_idx].intersection(guest_sets[idx])]
+        for collection_idx in indices
+    ]
+    return pd.Series(allowable_tablemates_lists, index=indices)
 
-def is_tabling_allowable(tabling,allowable_tablemates_lists):
+def is_tabling_allowable(tabling, allowable_tablemates_lists):
     """
     `tabling` is a pandas Series of cluster labels for the collection items. Returns True if the
     clustering satisfies the Banquet Table constraint, and False otherwise. Some cluster labels
     will be -1, indicating that the collection item is placed in the "noise" cluster by DBSCAN.
     Noise items are not subject to the Banquet Table constraint, because they are not tabled.
-
-    Some cluster labels will be null, because the collection item is a lonely or an outsider and 
-    is not entered into DBSCAN at all. We ignore these items when checking the Banquet Table
-    constraint.
     """
-
-    # Get a list of all clusters that are neither noise nor null
-    tabling = tabling.loc[tabling.notnull()]
-    tabling = tabling.loc[tabling != -1]
+     
     tables = tabling.unique()
+    tables = tables[tables != -1]
 
-    # Check the Banquet Table constraint for each table
     for table in tables:
-        table_members = tabling.loc[tabling == table].index
-        for table_member in table_members:
-            allowable_tablemates = allowable_tablemates_lists[table_member]
-            if len(set(table_members).intersection(set(allowable_tablemates))) == 0:
+        table_members_set = set(tabling[tabling == table].index)
+        for member in table_members_set:
+            # Remove the current member from the table members set for this check
+            candidate_tablemates = table_members_set - {member}
+            if not candidate_tablemates.issubset(allowable_tablemates_lists.loc[member]):
                 return False
-            
+
     return True
 
-def banquet_tabling(collection_frame,reference_frame,feature_columns,d,eps=0.164,eps_increment=0.0001):
+def banquet_tabling(collection_frame, reference_frame, feature_columns, d, eps=0.164, eps_increment=0.0001):
     """
     Returns a pandas Series (with `collection_frame` indices) of cluster labels for the collection 
     items, where the clustering satisfies the Banquet Table constraint. Lonelies and outsiders are
     not included in the clustering.
     """
 
-    guest_lists = get_guest_lists(collection_frame,reference_frame,feature_columns,d)
+    guest_lists = get_guest_lists(collection_frame, reference_frame, feature_columns, d)
     allowable_tablemates_lists = get_allowable_tablemates_lists(guest_lists)
 
-    lonelies = guest_lists.loc[guest_lists.apply(len) == 0].index
-    outsiders = allowable_tablemates_lists.loc[allowable_tablemates_lists.apply(len) == 0].index
+    lonelies = guest_lists[guest_lists.str.len() == 0].index
+    outsiders = allowable_tablemates_lists[allowable_tablemates_lists.str.len() == 0].index.difference(lonelies)
     attendees = collection_frame.index.difference(lonelies.union(outsiders))
 
-    tabling = cluster(collection_frame[feature_columns].loc[attendees],
-                      method='dbscan',eps=eps,min_samples=2)
-
-    while is_tabling_allowable(tabling,allowable_tablemates_lists) == False:
+    tabling = None
+    while True:
+        tabling = cluster(collection_frame[feature_columns].loc[attendees], method='dbscan', eps=eps, min_samples=2)
+        if is_tabling_allowable(tabling, allowable_tablemates_lists):
+            break
         eps -= eps_increment
-        tabling = cluster(collection_frame[feature_columns].loc[attendees],
-                      method='dbscan',eps=eps,min_samples=2)
-        
-    tabling_with_lonelies_and_outsiders = pd.Series(index=collection_frame.index)
-    tabling_with_lonelies_and_outsiders.loc[attendees] = tabling
-    tabling_with_lonelies_and_outsiders.loc[lonelies] = 'lonely'
-    tabling_with_lonelies_and_outsiders.loc[outsiders] = 'outsider'
-        
+
+    tabling_with_lonelies_and_outsiders = pd.Series(index=collection_frame.index, dtype=object)
+    tabling_with_lonelies_and_outsiders[attendees] = tabling
+    tabling_with_lonelies_and_outsiders[lonelies] = 'lonely'
+    tabling_with_lonelies_and_outsiders[outsiders] = 'outsider'
+    
     return tabling_with_lonelies_and_outsiders
